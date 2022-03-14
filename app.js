49bdc5552703278a5ca786f7e8647bb61282b02f
Rohitvpatil0810 <rohitvpatil0810@gmail.com>
2022-03-14 16:07:08 +0530

    Final Version3

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.use(eventRoutes);
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  const path = require("path");
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
