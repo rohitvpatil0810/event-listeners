4c75fb73cec2bf1cbcb3d223c40169da6f7120ac
Rohitvpatil0810 <rohitvpatil0810@gmail.com>
2022-03-14 15:58:06 +0530

    Final Version1

--- app.js
+++ app.js
@@ -37,4 +37,7 @@ app.use(eventRoutes);
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
